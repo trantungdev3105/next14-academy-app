c33177d51989186afa1c9921b37996e1cc237eae
trantungdev3105 <trantungdev3105@gmail.com>
2025-06-09 13:49:51 +0700

    feat: next font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 /** @format */
 
-import Image from "next/image";
+import { manrope, roboto } from "@/components/font";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="">
+      <h1>This is title</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eos, veniam
+        similique quis delectus voluptatem dolorum repudiandae repellendus quas,
+        maiores vero cumque. Quae asperiores, ab deleniti quia modi enim quasi?
+      </div>
+    </main>
   );
 }
