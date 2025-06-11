3eee2e712998cc9176117886e76d4af5aabdde0a
trantungdev3105 <trantungdev3105@gmail.com>
2025-06-11 10:40:30 +0700

    feat: dựng layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 /** @format */
 
-import { manrope, roboto } from "@/components/font";
-
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="font-secondary">This is title</h1>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eos, veniam
-        similique quis delectus voluptatem dolorum repudiandae repellendus quas,
-        maiores vero cumque. Quae asperiores, ab deleniti quia modi enim quasi?
-      </div>
-    </main>
-  );
+    <div className="px-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eos, veniam
+      similique quis delectus voluptatem dolorum repudiandae repellendus quas,
+      maiores vero cumque. Quae asperiores, ab deleniti quia modi enim quasi?
+    </div>
+  )
 }
